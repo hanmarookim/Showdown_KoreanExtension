22cf63d2189ec498151d465d431ec6c7873d2920
Maroo KH <hanmaroo.kim@gmail.com>
2020-12-11 23:59:27 +0900

    korean

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
 chrome.tabs.executeScript({
-  file: 'data/data_pokedex.js'
-});
-chrome.tabs.executeScript({
-  file: 'data/data_abilities.js'
-});
-chrome.tabs.executeScript({
-  file: 'data/data_items.js'
-});
-chrome.tabs.executeScript({
-  file: 'data/data_skills.js'
+  file: 'override.js'
 });
 chrome.tabs.executeScript({
   file: 'content.js'
